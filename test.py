5f08b9542ed329a44d4b59e3775abc9003430224
mjyplusone <mjyplusone@qq.com>
2016-12-10 22:31:29 +0800

    mail

--- test.py
+++ test.py
@@ -1,12 +1,16 @@
 from flask import Flask,render_template
-from flask.ext.bootstrap import Bootstrap
-from flask.ext.moment import Moment
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 from datetime import datetime
-from flask.ext.wtf import Form
+from flask_wtf import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 from flask import session, redirect, url_for, flash
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail
+from flask_script import Manager, Shell
+from flask_mail import Mail, Message
+from threading import Thread
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -15,10 +19,23 @@ app = Flask(__name__)
 app.config['SECRET_KEY']='hard to guess string'
 app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=True
 
+app.config['MAIL_SERVER'] = 'smtp.126.com'
+app.config['MAIL_PORT'] = 25
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+app.config['MAIL_USERNAME'] = 'mjytest@126.com'
+app.config['MAIL_PASSWORD'] = 'test123'
+app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[Flasky]'
+app.config['FLASKY_MAIL_SENDER'] = 'Flasky Admin <mjytest@126.com>'
+app.config['FLASKY_ADMIN']='mjytest@126.com'
+
+manager = Manager(app)
 db = SQLAlchemy(app)
-bootstrap=Bootstrap(app)
-moment=Moment(app)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+mail = Mail(app)
 
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -40,8 +57,13 @@ class User(db.Model):
         return '<User %r>' % self.username
 
 class NameForm(Form):
-    name=StringField('What is your name?',validators=[Required()])
-    submit=SubmitField('Submit')
+    name = StringField('What is your name?',validators=[Required()])
+    submit = SubmitField('Submit')
+    
+    
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Role=Role)
+manager.add_command("shell", Shell(make_context=make_shell_context))
 
 # @app.route('/')
 # def index():
@@ -67,8 +89,20 @@ class NameForm(Form):
         # return redirect(url_for('index'))
     # return render_template('index.html',
         # form = form, name = session.get('name'))
-        
-        
+ 
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
+
+def send_email(to, subject, template, **kwargs):
+    msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + ' ' + subject, sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
+    msg.body = render_template(template + '.txt', **kwargs)
+    msg.html = render_template(template + '.html', **kwargs)
+    thr = Thread(target=send_async_email, args=[app, msg])
+    thr.start()
+    return thr
+    
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
@@ -78,12 +112,16 @@ def index():
             user = User(username=form.name.data)
             db.session.add(user)
             session['known'] = False
+            if app.config['FLASKY_ADMIN']:
+                send_email(app.config['FLASKY_ADMIN'], 'New User', 'mail/new_user', user=user)
         else:
             session['known'] = True
         session['name'] = form.name.data
+        form.name.data = ''
         return redirect(url_for('index'))
     return render_template('index.html', form=form, name=session.get('name'),
                            known=session.get('known', False))
+                           
     
 @app.route('/<name>')
 def user(name):
